46252c40dd1bf4dd4ba84faf983bdf71a7d01777
sam <shengpengxiang1@outlook.com>
2026-03-11 17:54:11 +0800

    feat(BSP): add LED driver for balance car project
    Add new BSP LED driver files (bsp_led.c and bsp_led.h) to provide LED control functionality for the balance car. The implementation includes functions to turn LEDs on/off for motor status indicators (MOTOR_L, MOTOR_R) and voltage level indicators (VOLTAGE_1, VOLTAGE_2, VOLTAGE_3). The driver uses HAL GPIO functions for hardware control and returns standardized error codes for invalid parameters. This modular LED driver will be used for visual status indication in the balance car system.

---
+++ BSP/bsp_led.c
@@ -0,0 +1,75 @@
+/**
+ * @file    bsp_led.c
+ * @author  sam
+ * @brief   Board Support Package LED driver implementation
+ *
+ * This file contains the LED control functions for the balance car project.
+ * It provides functions to turn LEDs on and off based on LED identifiers.
+ */
+
+#include "bsp_led.h"
+#include "bsp_uart.h"
+#include "stm32f1xx_hal.h"
+#include "settings.h"
+#include <stdint.h>
+#include "bsp_config.h"
+
+/**
+ * @brief  Turn on the specified LED
+ * @param  led: LED identifier (MOTOR_L, MOTOR_R, VOLTAGE_1, VOLTAGE_2, VOLTAGE_3)
+ * @retval Return code indicating success or error
+ */
+uint32_t LedLightOn(LED_ID led)
+{
+  switch (led)
+  {
+  case MOTOR_L:
+    // Code to turn on MOTOR_L LED
+    break;
+  case MOTOR_R:
+    // Code to turn on MOTOR_R LED
+    break;
+  case VOLTAGE_1:
+    HAL_GPIO_WritePin(BSP_LED_VOLTAGE_1_GPIO_PORT, BSP_LED_VOLTAGE_1_PIN, GPIO_PIN_SET);
+    break;
+  case VOLTAGE_2:
+    HAL_GPIO_WritePin(BSP_LED_VOLTAGE_2_GPIO_PORT, BSP_LED_VOLTAGE_2_PIN, GPIO_PIN_SET);
+    break;
+  case VOLTAGE_3:
+    HAL_GPIO_WritePin(BSP_LED_VOLTAGE_3_GPIO_PORT, BSP_LED_VOLTAGE_3_PIN, GPIO_PIN_SET);
+    break;
+  default:
+    return MAKE_RETURN_CODE(ERROR, ERROR_LEVEL_MAJOR, MODULE_LED, ERR_TYPE_INVALID_PARAM, 0x01);
+  }
+  return MAKE_RETURN_CODE(SUCCESS, ERROR_LEVEL_MINOR, MODULE_LED, ERR_TYPE_SUCCESS, 0x00);
+}
+
+/**
+ * @brief  Turn off the specified LED
+ * @param  led: LED identifier (MOTOR_L, MOTOR_R, VOLTAGE_1, VOLTAGE_2, VOLTAGE_3)
+ * @retval Return code indicating success or error
+ */
+uint32_t LedLightOff(LED_ID led)
+{
+  switch (led)
+  {
+  case MOTOR_L:
+    // Code to turn off MOTOR_L LED
+    break;
+  case MOTOR_R:
+    // Code to turn off MOTOR_R LED
+    break;
+  case VOLTAGE_1:
+    HAL_GPIO_WritePin(BSP_LED_VOLTAGE_1_GPIO_PORT, BSP_LED_VOLTAGE_1_PIN, GPIO_PIN_RESET);
+    break;
+  case VOLTAGE_2:
+    HAL_GPIO_WritePin(BSP_LED_VOLTAGE_2_GPIO_PORT, BSP_LED_VOLTAGE_2_PIN, GPIO_PIN_RESET);
+    break;
+  case VOLTAGE_3:
+    HAL_GPIO_WritePin(BSP_LED_VOLTAGE_3_GPIO_PORT, BSP_LED_VOLTAGE_3_PIN, GPIO_PIN_RESET);
+    break;
+  default:
+    return MAKE_RETURN_CODE(ERROR, ERROR_LEVEL_MAJOR, MODULE_LED, ERR_TYPE_INVALID_PARAM, 0x01);
+  }
+  return MAKE_RETURN_CODE(SUCCESS, ERROR_LEVEL_MINOR, MODULE_LED, ERR_TYPE_SUCCESS, 0x00);
+}
---
+++ BSP/bsp_led.h
@@ -0,0 +1,41 @@
+/**
+ * @file    bsp_led.h
+ * @author  sam
+ * @brief   Board Support Package LED driver header
+ *
+ * This file contains the LED driver interface definitions for the balance car project.
+ * It defines LED identifiers and function prototypes for LED control.
+ */
+
+#ifndef BSP_LED_H
+#define BSP_LED_H
+
+#include <stdint.h>
+
+/**
+ * @brief  LED identifiers
+ */
+typedef enum
+{
+  MOTOR_L = 0,   /**< Left motor status LED */
+  MOTOR_R = 1,   /**< Right motor status LED */
+  VOLTAGE_1 = 2, /**< Voltage level 1 indicator LED */
+  VOLTAGE_2 = 3, /**< Voltage level 2 indicator LED */
+  VOLTAGE_3 = 4, /**< Voltage level 3 indicator LED */
+} LED_ID;
+
+/**
+ * @brief  Turn on the specified LED
+ * @param  led: LED identifier(choosed from LED_ID)
+ * @retval Return code indicating success or error
+ */
+uint32_t LedLightOn(LED_ID led);
+
+/**
+ * @param  led: LED identifier(choosed from LED_ID)
+ * @brief  Turn off the specified LED
+ * @retval Return code indicating success or error
+ */
+uint32_t LedLightOff(LED_ID led);
+
+#endif /* BSP_LED_H */
